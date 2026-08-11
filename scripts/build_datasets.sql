c4b4b7bfa98f31c52aa13e925a9806cb4c31eb1e
bhuang0022 <bhuang0022@gmail.com>
2026-08-11 03:44:36 -0700

    Add files via upload

---
+++ scripts/build_datasets.sql
@@ -0,0 +1,18 @@
+INSTALL httpfs; LOAD httpfs;
+CREATE TEMP MACRO r(p) AS
+  'https://storage.googleapis.com/calcofi-db/ducklake/releases/__RELEASE__/parquet/' || p;
+
+COPY (
+  SELECT provider || '_' || dataset AS dataset_key,
+         dataset_name,
+         CASE provider || '_' || dataset
+           WHEN 'cce-lter_euphausiids' THEN 'https://portal.edirepository.org/nis/mapbrowse?scope=knb-lter-cce&identifier=313'
+           WHEN 'sio_pic-zooplankton' THEN 'https://erddap.calcofi.io/erddap/tabledap/sio_pic-zooplankton_sample.html'
+           WHEN 'calcofi_phyllosoma' THEN 'https://portal.edirepository.org/nis/mapbrowse?packageid=knb-lter-cce.188.4'
+           ELSE coalesce(nullif(link_data_source, ''), nullif(link_calcofi_org, ''), nullif(link_others, ''))
+         END AS url,
+         link_calcofi_org, link_data_source, link_others,
+         description, citation_main, license, pi_names
+  FROM read_parquet(r('dataset.parquet'))
+  ORDER BY dataset_key
+) TO 'public/data/datasets_meta.json' (FORMAT JSON, ARRAY true);
